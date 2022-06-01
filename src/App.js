fd137664b824ceb866cb39172822cd85892f691d
godfather-coder <sandrokika74@gmail.com>
2022-06-01 16:57:40 +0400

    a

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+import NavBar from './navigationbar/NavBar';
 import './App.css';
+import "./navigationbar/styles.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          We Can Do IT!!!!!!
-        </a>
-      </header>
+     <NavBar />
     </div>
   );
 }
---
+++ src/navigationbar/NavBar.jsx
@@ -0,0 +1,25 @@
+import "./styles.css"
+import logo from './fds.jpg';
+ 
+ export default function NavBar(){
+    return (
+        <nav classname="nav">
+           
+
+            <a href="/" calssname="site-name">Project</a>
+            <ul classname="header"> 
+                <img src={logo} alt="Logo" />
+                <li classname = 'nav-elemnts'>
+                    <a herf ="Languages">Language</a>
+                    <a herf = "cart"> Cart</a>
+                    <a herf ="Sign Up"> Sign Up</a>
+                    <a href ="Logn in" > Log In</a>                    
+                </li>
+
+
+
+            </ul>
+        </nav>
+        
+    )
+ }
