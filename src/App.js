7150c21be03622f4f50c575e5b33fa23de6c9527
godfather-coder <sandrokika74@gmail.com>
2022-06-08 22:05:30 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import NavBar from './navigationbar/NavBar';
+import NavBar from './navigationbar/NavBar'
 import './App.css';
 import "./navigationbar/styles.css"
+import Footer from './footer/footer'
 
 function App() {
   return (
     <div className="App">
      <NavBar />
+
+
+
+
+
+
+     <Footer/>
     </div>
+
   );
 }
 
