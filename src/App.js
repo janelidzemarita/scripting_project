68374e41082146b2c880d397c76f7e3f5510f20b
M.J <janelidze.marita@kiu.edu.ge>
2022-06-30 14:54:50 +0400

    Update 14:54

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import NavBar from './navigationbar/NavBar'
+import React from 'react';
 import './App.css';
 import Slider from "./slide/Slide.jsx";
-import "./navigationbar/styles.css"
-import Form from './navigationbar/Login.jsx'
+import Form from './pages/Login.jsx'
 import Footer from './footer/footer'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+import Contact from './pages/contact';
 
 function App() {
   return (
   <Router>
     <div className="App">
-       <NavBar/>
+       <Navbar/>
        <Slider/>
        <Footer/>
-
-        <Route  path="home" element={<Footer/>} />
-        <Route  path="/navigationbar/Login.jsx" element={<Form/>} />
-
+            <Routes>
+                <Route exact path='/' exact element={<Home />} />
+                <Route path='/about' element={<About/>} />
+                <Route path='/contact' element={<Contact/>} />
+                <Route path='/blogs' element={<Blogs/>} />
+                <Route path='/sign-up' element={<SignUp/>} />
+                <Route  path="/pages/Login.jsx" element={<Form/>} />
+                <Route  path="home" element={<Footer/>} />
+            </Routes>
     </div>
- </Router >
-  );
+  </Router>
+);
 }
 
+
 export default App;
+
+
