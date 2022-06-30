bd9aef29260de4ad75ad99323496863f143034b6
godfather-coder <84451463+godfather-coder@users.noreply.github.com>
2022-06-30 17:45:42 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
-import Slider from "./slide/Slide.jsx";
+
 import Form from './pages/Login.jsx'
 import Footer from './footer/footer'
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route}
     from 'react-router-dom';
-import Home from './pages';
+import Home from './pages/home.jsx';
 import About from './pages/about';
 import Blogs from './pages/blogs';
-import SignUp from './pages/signup';
+import SignUp from './pages/Login.jsx';
 import Contact from './pages/contact';
 
 function App() {
   return (
   <Router>
     <div className="App">
-       <Navbar/>
-       <Slider/>
+
             <Routes>
-                <Route exact path='/' exact element={<Home />} />
+                <Route exact path='/'  element={<Home />} />
                 <Route path='/about' element={<About/>} />
                 <Route path='/contact' element={<Contact/>} />
                 <Route path='/blogs' element={<Blogs/>} />
@@ -28,7 +27,7 @@ function App() {
                 <Route  path="/pages/Login.jsx" element={<Form/>} />
                 <Route  path="home" element={<Footer/>} />
             </Routes>
-        <Footer/>
+        
     </div>
   </Router>
 );
@@ -36,5 +35,3 @@ function App() {
 
 
 export default App;
-
-
